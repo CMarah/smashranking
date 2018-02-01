df0c476477293c0f3aebbc169ea50954521aee5d
Guillermo Bandres <gbandres98@gmail.com>
2018-02-01 11:30:48 +0100

    Basic ranking method

--- index.js
+++ index.js
@@ -3,18 +3,18 @@ const fetch = require('isomorphic-fetch');
 const getPlayersPage = async (page, num_pages) => {
 	console.log(page);
 	if (page > num_pages) return [];
-	const resp = await fetch('http://smasharchive.eu/api/v0.1/players/?limit=100&page='+page).then(response => response.json());
-	const next = await getPlayersPage(page+1, num_pages);
+	const resp = await fetch('http://smasharchive.eu/api/v0.1/players/?limit=100&page=' + page).then(response => response.json());
+	const next = await getPlayersPage(page + 1, num_pages);
 	return resp.data.concat(next);
 };
 
 const getAllPlayerSlugs = async () => {
 	const total = (await fetch('http://smasharchive.eu/api/v0.1/players/?limit=1').then(response => response.json())).pagination.total;
 	console.log('total players: ', total);
-	const num_pages = Math.ceil(total/100);
+	const num_pages = Math.ceil(total / 100);
 	console.log(num_pages);
-	const player_urls = (new Array(num_pages)).fill(0).map((x,i,arr) =>
-		'http://smasharchive.eu/api/v0.1/players/?limit=100&page='+i
+	const player_urls = (new Array(num_pages)).fill(0).map((x, i, arr) =>
+		'http://smasharchive.eu/api/v0.1/players/?limit=100&page=' + i
 	);
 	console.log(player_urls);
 
@@ -27,18 +27,18 @@ getAllPlayerSlugs();
 const getTournamentPage = async (page, num_pages) => {
 	console.log(page);
 	if (page > num_pages) return [];
-	const resp = await fetch('http://smasharchive.eu/api/v0.1/players/?limit=100&page='+page).then(response => response.json());
-	const next = await getPlayersPage(page+1, num_pages);
+	const resp = await fetch('http://smasharchive.eu/api/v0.1/players/?limit=100&page=' + page).then(response => response.json());
+	const next = await getPlayersPage(page + 1, num_pages);
 	return resp.data.concat(next);
 };
 
 const getAllTournaments = async () => {
 	const total = (await fetch('http://smasharchive.eu/api/v0.1/tournaments/?limit=1').then(response => response.json())).pagination.total;
 	console.log('total tournaments: ', total);
-	const num_pages = Math.ceil(total/100);
+	const num_pages = Math.ceil(total / 100);
 	console.log(num_pages);
-	const player_urls = (new Array(num_pages)).fill(0).map((x,i,arr) =>
-		'http://smasharchive.eu/api/v0.1/players/?limit=100&page='+i
+	const player_urls = (new Array(num_pages)).fill(0).map((x, i, arr) =>
+		'http://smasharchive.eu/api/v0.1/players/?limit=100&page=' + i
 	);
 	console.log(player_urls);
 
--- rankings.js
+++ rankings.js
@@ -1,1 +1,19 @@
-//TODO
+fs = require('fs');
+
+function rank(players, sets) {
+    players.map(x => x.skill = [25.0, 25.0 / 3.0]); //Initialize Trueskill data (mu 25, sigma 25/3)
+    sets.map(x => { //Apply set results
+        winner = players.find(y => y.id == x.winner);
+        loser = players.find(y => y.id == x.loser);
+        XWinsOverY(winner, loser);
+    })
+    players.map(x => x.score = (x.skill[0] - 3 * x.skill[1])); //Calculate score
+    players.sort((x, y) => x.skill - y.skill); //Order by score
+    fs.writeFile('ranking.json', players);
+}
+
+function XWinsOverY(winner, loser) {
+    winner.rank = 1;
+    loser.rank = 2;
+    trueskill.AdjustPlayers(winner, loser);
+}
